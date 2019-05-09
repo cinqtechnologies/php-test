26c661c46498411081601a8744f963bb8d6d7807
Marco Moura <marco.moura@fatec.sp.gov.br>
2019-05-08 21:47:51 -0300

    Base entities and respective test classes added

---
+++ api/app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 08/05/19
+ * Time: 21:18
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        "name",
+        "price",
+        "image",
+        "retailerId",
+        "description"
+    ];
+
+    public function retailer() {
+        return $this->belongsTo('App\Models\Retailer', 'retailerId');
+    }
+}
---
+++ api/app/Models/Retailer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 08/05/19
+ * Time: 21:05
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Retailer extends Model
+{
+    protected $fillable = [
+        "name",
+        "logo",
+        "description",
+        "website"
+    ];
+
+    public function products() {
+        return $this->hasMany('App\Product', 'retailerId');
+    }
+}
