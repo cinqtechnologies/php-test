b6ff70d574a9bef606aebc9762ef6058eae6e18b
Marco Moura <marco.moura@fatec.sp.gov.br>
2019-05-10 01:33:23 -0300

    Base Service added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RetailerListComponent } from './pages/retailer-list/retailer-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'retailers', component: RetailerListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
