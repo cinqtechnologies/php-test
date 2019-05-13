fa3efe17c5bd22ad1c15378a0f9cb51485357e93
Marco Moura <marco.moura@fatec.sp.gov.br>
2019-05-13 05:33:51 -0300

    Images target directory changed

--- api/app/Lib/Image.php
+++ api/app/Lib/Image.php
@@ -9,11 +9,11 @@ class Image
 
         $filename = md5(date('Y-m-d h:i:s'));
         $extension = substr($fileTyype, strpos($fileTyype, "/") + 1);
-        $filename .= ".".$extension;
+        $fullPath = "images/".$filename.".".$extension;
 
-        if(file_put_contents($filename, base64_decode($blob)))
+        if(file_put_contents($fullPath, base64_decode($blob)))
         {
-            return $filename;
+            return $fullPath;
         }
 
         return false;
